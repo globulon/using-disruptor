59dded48fabeef3b154378138c85afffab2a99ac
Marc-Daniel Ortega <globulon@gmail.com>
2012-02-02 20:24:41 +0100

    first steps in creating a scala batch event processor. First shot but I do not like the result

--- src/main/scala/com/promindis/disruptor/port/BatchEventProcessor.scala
+++ src/main/scala/com/promindis/disruptor/port/BatchEventProcessor.scala
@@ -16,31 +16,41 @@ final case class BatchEventProcessor[T](ringBuffer: RingBuffer[T], sequenceBarri
     sequenceBarrier.alert()
   }
 
-  def safely[T](f: => T) = {
+  def alert: PartialFunction[Throwable, Option[Long]] = {
+    case ex: AlertException if (!running.get())=> None
+  }
+
+  def trap[E](sequence: Long, forEvent: E): PartialFunction[Throwable, Option[Long]] = {
+    case ex: Throwable =>
+      print("exception raised")
+      exceptionHandler.handleEventException(ex, sequence, forEvent)
+      Some(sequence)
+  }
+
+  def managing(trap: => PartialFunction[Throwable, Option[Long]])(f: => Long): Option[Long] = {
     try {
       Some(f)
-    } catch {
-      case ex: AlertException if (!running.get())=> None
-    }
+    } catch trap
   }
 
   def nextSequence(l: Long) = {
-    safely{
+    managing (alert){
       sequenceBarrier.waitFor(l)
     }
   }
 
-  def handle(nextSequence: Long, asLast: Boolean) {
-    val event = ringBuffer.get(nextSequence)
-    eventHandler.onEvent(event, nextSequence, asLast)
+  def handlingEvent(sequence: Long, asLast: Boolean): Option[Long] = {
+    val event = ringBuffer.get(sequence)
+    managing(alert orElse trap(sequence, event)) {
+      eventHandler.onEvent(event, sequence, asLast)
+      sequence
+    }
   }
 
   def handleEvents(nextSequence: Long, availableSequence: Long): Option[Long]  = {
-    for (index <- nextSequence to availableSequence){
-      handle(nextSequence, nextSequence == availableSequence)
-    }
-    Some(availableSequence)
-
+    Some((nextSequence to availableSequence).dropWhile{ index =>
+      handlingEvent(index, index == availableSequence).isDefined
+    }.headOption.getOrElse(availableSequence))
   }
 
   def loop() {
@@ -53,10 +63,10 @@ final case class BatchEventProcessor[T](ringBuffer: RingBuffer[T], sequenceBarri
     }
 
     @tailrec def innerLoop(fromIndex: Long) {
-      val nextIndex = processEvents(fromIndex)
-      if (nextIndex.isDefined)  {
-        sequence.set(nextIndex.get)
-        innerLoop(nextIndex.get + 1)
+      val lastIndex = processEvents(fromIndex)
+      if (lastIndex.isDefined)  {
+        sequence.set(lastIndex.get)
+        innerLoop(lastIndex.get + 1)
       }
     }
 
